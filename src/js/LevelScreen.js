e238da2c8f07259dd91ba0089dc439b5dffcd6f2
Elliot Nelson <nelson.work@gmail.com>
2023-09-09 14:28:42 -0400

    sea level

--- src/js/LevelScreen.js
+++ src/js/LevelScreen.js
@@ -20,6 +20,7 @@ import { ScreenShake } from './ScreenShake';
 import { BigArrowParticle } from './BigArrowParticle';
 import { FallingDirtParticle } from './FallingDirtParticle';
 import { CloudParticle } from './CloudParticle';
+import { StarParticle } from './StarParticle';
 
 export class LevelScreen {
     constructor(levelNumber) {
@@ -36,6 +37,10 @@ export class LevelScreen {
 
         this.player = new Player(qr2xy({ q: this.levelData.spawn[0], r: this.levelData.spawn[1] }));
         this.addEntity(this.player);
+        this.addEntity(new StarParticle(this.player.pos));
+        this.addEntity(new StarParticle(this.player.pos));
+        this.addEntity(new StarParticle(this.player.pos));
+        this.addEntity(new StarParticle(this.player.pos));
         this.addEntity(new CloudParticle(true));
 
         this.littlePigs = 0;
@@ -121,21 +126,21 @@ export class LevelScreen {
         Viewport.ctx.fillStyle = '#457cd6';
         Viewport.ctx.fillRect(0, 0, Viewport.width, Viewport.height);
 
-        let bottomSea = 5;
-        let topSea = 40;
+        let bottomSea = 4;
+        let topSea = 14;
         let percentage = Math.floor(clamp(this.player.pos.y / (this.tiles.length * TILE_SIZE), 0, Infinity) * (topSea - bottomSea) + bottomSea);
 
         Viewport.ctx.fillStyle = '#4b3b9c';
         Viewport.ctx.fillRect(0, Viewport.height - percentage, Viewport.width, percentage);
 
-        Viewport.ctx.fillStyle = '#8fcccb';
+        /*Viewport.ctx.fillStyle = '#8fcccb';
         Viewport.ctx.fillRect(0, Viewport.height - percentage - 2, Viewport.width, 1);
 
         Viewport.ctx.fillStyle = '#449489';
         Viewport.ctx.fillRect(0, Viewport.height - percentage - 4, Viewport.width, 1);
 
         Viewport.ctx.fillStyle = '#285763';
-        Viewport.ctx.fillRect(0, Viewport.height - percentage - 6, Viewport.width, 1);
+        Viewport.ctx.fillRect(0, Viewport.height - percentage - 6, Viewport.width, 1);*/
 
         // Render screenshakes (canvas translation)
         let shakeX = 0, shakeY = 0;
